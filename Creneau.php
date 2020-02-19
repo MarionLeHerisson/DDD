c2cdc1b032f172193a8356deca214b7d2e01dfbd
MarionLeHerisson <marion.hurteau1@gmail.com>
2020-02-19 12:02:39 +0100

    add right files

--- Creneau.php
+++ Creneau.php
@@ -1,1 +1,65 @@
 <?php
+
+namespace DDD;
+
+class Creneau {
+
+    private $date;
+    private $heureDebut;
+    private $heureFin;
+
+    /**
+     * @return false|string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param false|string $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return false|string
+     */
+    public function getHeureDebut()
+    {
+        return $this->heureDebut;
+    }
+
+    /**
+     * @param false|string $heureDebut
+     */
+    public function setHeureDebut($heureDebut)
+    {
+        $this->heureDebut = $heureDebut;
+    }
+
+    /**
+     * @return false|int|string
+     */
+    public function getHeureFin()
+    {
+        return $this->heureFin;
+    }
+
+    /**
+     * @param false|int|string $heureFin
+     */
+    public function setHeureFin($heureFin)
+    {
+        $this->heureFin = $heureFin;
+    }
+
+    public function __construct($dateTime, $duree)
+    {
+        $this->heureDebut = gmdate("H:i:s", $dateTime);
+        $this->date = gmdate("d/m/Y", $dateTime);
+        $this->heureFin = strtotime('+'.$duree.' hours', strtotime($this->heureDebut));
+    }
+}
--- Tests/CreneauTest.php
+++ Tests/CreneauTest.php
@@ -1,1 +1,24 @@
 <?php
+
+use DDD\Creneau;
+use \PHPUnit\Framework\TestCase;
+require __DIR__ . "/../Creneau.php";
+
+class CreneauTest extends TestCase {
+
+    private $creneau;
+
+    public function setUp(): void
+    {
+        $this->creneau = new Creneau(1582450200, 2);
+    }
+
+    // début < fin
+    public function testHeure() {
+        self::assertTrue($this->creneau->getHeureDebut() < $this->creneau->getHeureFin());
+    }
+
+    public function testDate() {
+        self::assertTrue($this->creneau->getDate() > date("d/m/Y"));
+    }
+}
